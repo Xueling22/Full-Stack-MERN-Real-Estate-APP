86081e8f6e00e6722bc898c80e7cbf6baa063f6d
Xueling22 <zuiainiwyw@163.com>
2024-03-25 20:27:36 +0000

    create route 4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-// import React from 'react'
+import {BrowserRouter, Routers,Router} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/Signin';
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routers>
+      <Router path='/' element={<Home />} />
+      <Router path='/sign-up' element={<SignUp />} />
+      <Router path='/sign-in' element={<SignIn />} />
+      <Router path='/about' element={<About />} />
+      <Router path='/profile' element={<Profile />} />
+    </Routers>
+    </BrowserRouter>
   )
 }
