e5d00493b10c4c8edaec31480e5073a6052437f5
Xueling22 <zuiainiwyw@163.com>
2024-03-26 03:22:20 +0000

    signin 15

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,10 @@
 import { error } from "console";
 import User from "../models/user.model.js"; //所有都不能漏了.js
 import bcryptjs from "bcryptjs"; //这个过程是单向的，意味着无法从散列值逆向推导出原始密码
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
+import { userInfo } from "os";
+import dotenv from 'dotenv'; //这个没有引不来env，无语
 
 export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
@@ -14,3 +18,29 @@ export const signup = async (req, res,next) => {
   }
   //保存到数据库需要时间，所以异步，存到mongodb数据库里了
 };
+
+export const signin= async (req, res,next) => {
+    const { email, password } = req.body;
+    try {
+      const validUser=await User.findOne({email:email}); //因为名一样也可以写({email})
+      if(!validUser) return next(errorHandler(404,'User not found'));
+      const validPassword=bcryptjs.compareSync(password,validUser.password); //比较两个密码。第一个参数是用户输入的密码（未加密），第二个参数是存储在数据库中的密码（已加密）。
+      if(!validPassword) return next(errorHandler(401,'Wrong credential!'));
+      const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+      const {password:hashedPassword,...rest}=validUser._doc;//._doc;代表包含数据库文档的纯数据，避免处理查询结果对象中的方法和其他非数据属性
+// 从validUser对象中提取password属性，并将其值赋给新的变量hashedPassword。这意味着，如果validUser对象中有一个属性叫password，它的值现在会被存储在hashedPassword变量中，而不是一个同名的password变量中。
+// ...rest是使用了剩余参数（rest parameter）语法，它将validUser对象中除了password之外的所有属性收集到一个新的对象rest中。这样，rest对象包含了validUser的所有属性和值，除了password。
+      jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+// 这一行使用jsonwebtoken库的sign方法来生成一个新的JWT。
+// 第一个参数是一个包含要编码在JWT中的数据的对象。在这个例子中，它包含了一个用户的唯一标识符id，这通常用于以后识别和验证用户。
+// 第二个参数是用于签名和验证JWT的密钥。这个密钥应该是一个只有服务器知道的秘密字符串，存储在环境变量JWT_SECRET中。这增加了安全性，因为即使JWT被拦截，没有密钥也无法伪造有效的JWT。
+      res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest); //只返回除了加密密码之外的 
+      //cookie里面包含的jwt，是可以识别用户唯一id的办法，就能确定是谁了，因为web识别不了请求来自于谁，cookie可以帮忙然后保存之前的状态，加载更丝滑
+// 这一行使用Express的响应对象res的cookie方法来设置一个名为access_token的cookie。
+// 第一个参数是cookie的名称。
+// 第二个参数是之前生成的JWT。
+// 第三个参数是一个选项对象，这里设置httpOnly: true使得这个cookie只能通过HTTP请求被访问，而不能通过客户端脚本访问。这是一个重要的安全措施，可以帮助减少跨站脚本攻击（XSS）的风险
+    } catch (error) {
+      next(error);
+    }
+  };
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,6 +1,80 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
-export default function Signin() {
+
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  //useState({})调用了useState函数，并且以一个空对象{}作为初始状态值。这意味着formData这个状态初始时是一个空对象，通常用来存储表单字段的值。
+// useState函数返回一个数组，这个数组包含两个元素：
+// 当前的状态值（在这个例子中是formData），它反映了状态的当前值。
+// 一个让你更新这个状态的函数（在这个例子中是setFormData），你可以调用这个函数来更新状态，更新状态后组件会重新渲染以反映最新的状态。
+  const [error, setError] = useState(null); //error初始值null
+  const [loading, setloading] = useState(false); //loading初始值false
+  const navigate=useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setloading(true); //异步操作要开始了
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      }); //请求的body是将formData状态转换为JSON字符串的结果。等待响应，然后将响应解析为JSON格式的数据。
+      const data = await res.json();
+      if (data.success === false) {
+        setloading(false);
+        setError(data.message);
+        return;
+      }
+      setloading(false);
+      setError(null); //清除之前的error信息
+      navigate('/');
+    } catch (error) {
+      setloading(false);
+      setError(error.message);
+    }
+  };
   return (
-    <div>Signin</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="text"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        ></input>
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Dont have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700">Sign up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }
