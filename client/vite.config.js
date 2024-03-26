574f94ab92afded1950901bdad601152087c9fdd
Xueling22 <zuiainiwyw@163.com>
2024-03-26 02:26:04 +0000

    sign up 13

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api':{ //每次看到api都自动加3000上去
+        target:'http://localhost:3000',
+        secure:false,
+      },
+    },
+  },
   plugins: [react()],
 })
